d1635bd538f49545f18a529e3fdb7fc6ec092c57
ibenthinkin <benjamin.reilly@gmail.com>
2019-09-11 16:26:38 -0600

    implement firebase unitls, add abilty to store authenticated users in firestore DB

--- src/App.js
+++ src/App.js
@@ -7,13 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import {auth} from './firebase/firebase.utils'
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 
 class App extends React.Component {
 
@@ -27,10 +21,30 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>{
-      this.setState({currentUser: user});
-      console.log(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+        if (userAuth) {
+          const userRef = await createUserProfileDocument(userAuth);
+
+          userRef.onSnapshot(snapShot => {
+            this.setState({
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            },
+              ()=>{console.log(this.state)}
+            )
+
+          })
+          
+        }
+        else {
+          this.setState({
+            currentUser: userAuth
+          })
+        }
+      
+    });
   }
 
   componentWillUnmount(){
@@ -46,7 +60,6 @@ class App extends React.Component {
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={ShopPage} />
             <Route path='/signin' component={SignInAndSignUp} />
-
           </Switch>
         </div>
       );
